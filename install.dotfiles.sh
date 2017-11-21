c0b9ce394c14179e392428aff9b2b0ace97f5f35
Maddison Hellstrom <maddy@thundr.net>
2017-11-21 08:53:47 -0800

    Only attempt to link if path !exists

--- install.dotfiles.sh
+++ install.dotfiles.sh
@@ -3,6 +3,7 @@
 [[ ! $DOTFILESINSTALL ]] && echo "Error: Must be installed with dotfiles/install.sh" && exit 1
 
 install_path="$VENDORDIR/spaceship-zsh-theme"
+link_path="$BASEDIR/zsh-custom/themes/spaceship.zsh-theme"
 
 [[ -d $install_path ]] && {
 	git clone https://github.com/denysdovhan/spaceship-zsh-theme.git $install_path
@@ -11,4 +12,6 @@ install_path="$VENDORDIR/spaceship-zsh-theme"
 	git pull origin master
 }
 
-ln -s $VENDORDIR/spaceship-zsh-theme/spaseship.zsh $BASEDIR/zsh-custom/themes/spaceship.zsh-theme
+[[ -e $link_path ]] && {
+	ln -s $VENDORDIR/spaceship-zsh-theme/spaseship.zsh $link_path
+}
